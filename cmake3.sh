8d6d30a2877b77d31d3d6282f4d403498b5765a6
Qruliq <137716873+Qruliq@users.noreply.github.com>
2023-07-10 21:13:40 +0200

    Create cmake3.sh

---
+++ cmake3.sh
@@ -0,0 +1,8 @@
+sudo apt-get install build-essential libssl-dev
+wget https://github.com/Kitware/CMake/releases/download/v3.25.0/cmake-3.25.0.tar.gz
+tar -zxvf cmake-3.25.0.tar.gz
+cd cmake-3.25.0
+./bootstrap
+make
+sudo make install
+cd ..
